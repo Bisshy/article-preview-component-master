0316c4aa60e2e62d8c01839f3e36d8e9a92da757
Bishop <oakinronbi@gmail.com>
2024-08-23 03:25:34 +0100

    hidden class updated
    visibility has been fixed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const iconBox = document.getElementById('icon-box')
 const shareIcon = document.getElementById('share-icon')
-
+let isShow = true
 
 shareIcon.addEventListener('click', function() {
-    iconBox.classList.toggle('hidden')
+   
+        iconBox.classList.toggle('hidden')
+ 
+    
 })
 
 
