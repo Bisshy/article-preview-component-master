542ddc443c3d0fb3e9f5d5e3969dc9a232166f9e
Bishop <oakinronbi@gmail.com>
2024-08-23 02:51:42 +0100

    final changes 1.0
    html css and js complete

---
+++ index.js
@@ -0,0 +1,11 @@
+const iconBox = document.getElementById('icon-box')
+const shareIcon = document.getElementById('share-icon')
+
+
+shareIcon.addEventListener('click', function() {
+    iconBox.classList.toggle('hidden')
+})
+
+
+
+
